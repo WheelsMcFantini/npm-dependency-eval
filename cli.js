4bab3e78e00d9664c91c49b94aafa6077f214d6a
Eden Mendel <mendeleden@gmail.com>
2021-12-12 19:14:44 -0500

    refactor: rename index.js to cli.js and add default value to --depth

--- cli.js
+++ cli.js
@@ -1,18 +1,22 @@
 //Creates a js object, and uses "destructuring assignment" to pull a value out of the output of commander
 //and pack it into the created object
-const { program, version } = require('commander');
+const { program } = require('commander');
 const { evalRunner }  = require('./src/evalRunner');
 program.version('0.1.0');
+const chalk = require('chalk')
 
-/*
-So, the following block defines properties of the program object that commander gave us
-It has a required option of packageName, and an option for a version
-When it recieves the --help argument, it prints out the help. 
-Once that code runs, I can pull the given arguments from it and run my program. 
-*/
+const cliLogger = (msg) => console.log(`${chalk.blue('[cli]')} ${msg}`);
+
+/**
+ * So, the following block defines properties of the program object that commander gave us
+ * It has a required option of packageName, and an option for a version
+ * When it recieves the --help argument, it prints out the help. 
+ * Once that code runs, I can pull the given arguments from it and run my program. 
+ */
 program
     .requiredOption('-p, --packageName <name>', 'package to evaulate the dependencies of')
-    .option('-d, --depth <max_depth_level>', 'depth of the dependency tree')
+    // Question: Should we default to get the entire dependecy tree? Should --depth only accept string or also numbers, both?
+    .option('-d, --depth <max_depth_level>', 'A numerical value of depth of the dependency tree', 'max')
     .option('-v, --packageVersion [version]',  "package version")
     .option('--debug', 'enables verbose output')
     .on('--help', function(){
@@ -25,7 +29,7 @@ program
         console.log("npm-dependency-eval -p neatPackage -v 0.0.0")
     })
     .action(async (options) => {
-        console.log(`[index] awaiting eval runner`)
+        cliLogger(`awaiting eval runner`)
         await evalRunner(options)
     })
 program.parse()
