7b510af204163f57a9179c3b57821fe42681099c
Eden Mendel <mendeleden@gmail.com>
2021-12-12 19:16:16 -0500

    refactor: potential rewrite of log statements and adding a few question

--- src/evalRunner.js
+++ src/evalRunner.js
@@ -1,49 +1,62 @@
 //importing commander
+const chalk = require('chalk')
 const { option } = require('commander')
+
 //importing the fucntions I exported with module.exports
-const { getLatestPackageVersion,
+const { 
+    getLatestPackageVersion,
     getDependencyList,
     fetchPackageInfo,
     getDependenciesOfDependencies } = require('./dependency-eval')
-const chalk = require('chalk')
+
+const evalLogger = (msg) => console.log(`${chalk.magenta('[eval-runner]')} ${msg}`);
 
 async function evalRunner(options) {
-    console.time("evalrunner")
+    evalLogger(`Started EvalRunner`);
+    console.time("evalrunner"); // Start timer to determine how long eval timer ran for
+
+    // Question: Should we verify if depth is a negative number?
     const { packageName, depth } = options
+    // Question: What to do when package version isn't a semantic version or latest?
     let { packageVersion } = options
 
-    console.log(`[evalRunner] package Name:  ${packageName}`)
-    //alternate if statement:
-    // if packageVersion exists, assign it;s value to itsself. 
+    evalLogger(`package Name: ${packageName}`);
+    // alternate if statement: if packageVersion exists --> assign it's value to itself, otherwise determine latest version 
     // packageVersion = packageVersion ? packageVersion : await getLatestPackageVersion(packageName);
 
     //logic to ensure we can feed a package name and version to getDependencyList
     if (packageVersion == undefined) {
-        console.log(`${chalk.red("[evalRunner]")} packageVersion was undefined`)
-        console.log(`${chalk.red("[evalRunner]")} Retrieving latest package version`)
+        evalLogger(`packageVersion was undefined, retrieving 'latest' information to extract package version.`)
         packageVersion = await getLatestPackageVersion(packageName)
-        console.log(`${chalk.green("[evalRunner]")} ${packageName} latest version is ${packageVersion}`)
+        evalLogger(`${packageName} latest version is ${packageVersion}`)
     } else {
-        console.log("[evalRunner] packageVersion was defined")
-        console.log(`[evalRunner] packageVersion: ${packageVersion}`)
+        evalLogger(`packageVersion was defined, packageVersion: ${packageVersion}`)
     }
-    //console.log(`${chalk.green("[evalRunner]")} retrieved package Version:  ${packageVersion}`);
 
     const requestedPackageInfo = await fetchPackageInfo(packageName, packageVersion);
-    console.log(`[evalRunner] ${requestedPackageInfo.name}`)
-    console.log(`[evalRunner] ${requestedPackageInfo.version}`)
-
+    
     const dependencies = await getDependencyList(requestedPackageInfo)
+    console.log(dependencies);
+    // Potential conversion to an array which is easier to iterate over
+    const arrayOfDependecies = Object.entries(dependencies)
+        .map( entry => {
+          const [name, version] = entry;
+          return {name, version}
+        })
+    console.log(arrayOfDependecies);
+    evalLogger(`Package Name: ${requestedPackageInfo.name}, Package Version: ${requestedPackageInfo.version}, has ${dependencies.length} dependecies.`)
 
     //output the dependencies programatically, eventually look up each one
     //dependency is an object like {'coolPackage/neat-feature': 4.2.0, etc}
     if (depth > 0) { 
         //create an object, get dependencies of dependencies ${depth} times and print them all
+        // Question: should we start the master dependecy tree with our requested package as the top item?
         masterDependencyTree = {}
         console.log(`[evalRunner] Depth: ${depth}`)
         for (let i = 0; i <= depth; i++) {
             console.log(`[evalRunner] Round: ${i}`)
             depKey = i
+            // Are we recursivley calling it for each package? I think we may be iterating and fetching the same URLs over and over
             masterDependencyTree[depKey] = await getDependenciesOfDependencies(dependencies)
         }
         console.log(`[evalRunner] Dependencies for ${packageName}:${packageVersion}:`)
