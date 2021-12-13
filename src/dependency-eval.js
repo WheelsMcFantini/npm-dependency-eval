31be537c34844b4513fa18bbb08f69bc9a6652c6
Eden Mendel <mendeleden@gmail.com>
2021-12-12 19:17:05 -0500

    refactor: examples of object destructing plus left to right assignment and additional questions

--- src/dependency-eval.js
+++ src/dependency-eval.js
@@ -1,17 +1,22 @@
 const { version } = require('commander')
 const https = require('https')
 const fetch = require('node-fetch')
-const VERSION = ""
 const REGISTRY_API = "registry.npmjs.org"
 
 async function fetchPackageInfo(packageName, packageVersion) {
   const url = `https://${REGISTRY_API}/${packageName}/${packageVersion}`
   console.log(`[dependency-eval] Fetching url: ${url}`)
-  const data = await fetch(url)
+  // Question: what do we fetch when a dep includes ^ or 0.0.x?
+  // Intresting enough ... registry api listed above, converts https://registry.npmjs.org/stack-trace/0.0.x and returns info for 0.0.x
+  // However, https://registry.npmjs.org/triple-beam/^1.3.0 does not translates to anything
+  
+  const data = await fetch(url) // Will this always be successful?
+  
   const parsedData = await data.json()
   return parsedData
 }
 
+// Question: Maybe we need a "resolve" package version which can return a package version for semvar expressions + latest
 async function getLatestPackageVersion(packageName) {
   const packageInfo = await fetchPackageInfo(packageName, 'latest')
   return packageInfo.version
@@ -19,11 +24,10 @@ async function getLatestPackageVersion(packageName) {
 
 
 async function getDependencyList(packageData) {
-  //const { name, version } = packageData
-  //console.log(`[dependency-eval] package Name:  ${packageData.name}`)
-  //console.log(`[dependency-eval] package Version:  ${packageData.version}`)
+  // neat trick for left to right assignment. the left are the keys inside `packageData` and the right are the consts we are creating 
+  const { name: packageName, version: packageVersion } = packageData
 
-  const url = `https://${REGISTRY_API}/${packageData.name}/${packageData.version}`
+  const url = `https://${REGISTRY_API}/${packageName}/${packageVersion}`;
   console.log(`[dependency-eval] URL: ${url}`)
   
   const data = await fetch(url)
@@ -34,18 +38,14 @@ async function getDependencyList(packageData) {
 
 async function getDependenciesOfDependencies(dependencies) {
   depsOfDeps = {}
-    for (const dependency in dependencies) {
-        console.log(`[dependency-eval] grabbing dependencies for ${dependency}:${dependencies[dependency]}`)
-        depKey = `${dependency}:${dependencies[dependency]}`
-        const name = dependency
-        const version = dependencies[dependency]
-        depsOfDeps[depKey] = await getDependencyList({ name, version})
-      }
+  for (const dependency in dependencies) {
+      console.log(`[dependency-eval] grabbing dependencies for ${dependency}:${dependencies[dependency]}`)
+      depKey = `${dependency}:${dependencies[dependency]}`
+      const name = dependency;
+      const version = dependencies[dependency];
+      depsOfDeps[depKey] = await getDependencyList({ name, version})
+    }
   return depsOfDeps
 }
 
-function convertToTree() {
-  console.log('[convertToTree] lol converting to tree')
-}
-
-module.exports = { getLatestPackageVersion, fetchPackageInfo, getDependencyList, getDependenciesOfDependencies, convertToTree}
+module.exports = { getLatestPackageVersion, fetchPackageInfo, getDependencyList, getDependenciesOfDependencies}
