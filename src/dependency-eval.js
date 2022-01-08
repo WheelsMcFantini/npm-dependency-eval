92636c2ac12498e7ff4e15990d31ec773ee5cb72
Eden Mendel <mendeleden@gmail.com>
2022-01-08 16:07:34 -0500

    refactor: pass packageName, packageVersion between functions instead of object

--- src/dependency-eval.js
+++ src/dependency-eval.js
@@ -1,8 +1,6 @@
-const { version } = require('commander')
-const https = require('https')
 const fetch = require('node-fetch')
-const VERSION = ""
-const REGISTRY_API = "registry.npmjs.org"
+
+const REGISTRY_API = "registry.npmjs.org" // Default registry to fetch packages from
 
 async function fetchPackageInfo(packageName, packageVersion) {
   const url = `https://${REGISTRY_API}/${packageName}/${packageVersion}`
@@ -18,55 +16,64 @@ async function getLatestPackageVersion(packageName) {
 }
 
 
-async function getDependencyList(packageData) {
-  let { name, version } = packageData
-  //console.log(`[dependency-eval] package Name:  ${packageData.name}`)
-  //console.log(`[dependency-eval] package Version:  ${packageData.version}`)
+function stripSemvarAnnotations(packageVersion) {
+  const cleanVersionNumber = packageVersion[0] === '^' ?  packageVersion.split('^')[1] : packageVersion;
+  return cleanVersionNumber;
+}
 
-  //if the first character of version is '^' 
-  //?
-  //then version = version.split('^')[1]
-  //: else
-  //version = version
-  version = version[0] === '^' ?  version.split('^')[1] : version
+async function getDependencyList(packageName, packageVersion) {
+  console.log(`[getDependencyList] get dependecy list for: ${packageName}@${packageVersion}`);
+  const cleanedPackageVersion = stripSemvarAnnotations(packageVersion);
+  const url = `https://${REGISTRY_API}/${packageName}/${cleanedPackageVersion}`
+  console.log(`[getDependencyList] attempting to fetch URL: ${url}`)
 
-  const url = `https://${REGISTRY_API}/${name}/${version}`
-  console.log(`[dependency-eval] URL: ${url}`)
+  let packageDependecies;
+  try {
+    const packageData = await fetch(url);
+    const parsedPackageData = await packageData.json();
+    // console.log(parsedPackageData);
+    packageDependecies = parsedPackageData.dependencies || {};
+    console.log(packageDependecies);
+  } catch (error) {
+    console.log(`[getDependencyList] Failed to fetch package data for ${packageName}@${cleanedPackageVersion}, aborting.`);
+    console.log(error);
+    throw new Error(`Failed to fetch package data for ${packageName}@${cleanedPackageVersion} from ${url}.`)
+  }
+
+  const dependeciesCount = Object.keys(packageDependecies).length;
+  console.log(`[getDependencyList] ${packageName}@${cleanedPackageVersion} has ${dependeciesCount} dependecies.`);
+  return packageDependecies;
+}
+
+/**
+ * 
+ * @param {*} packageName 
+ * @param {*} packageVersion 
+ * @param {*} depth 
+ * @returns 
+ */
+async function recursiveRoutine(packageName, packageVersion, depth){
+  console.log(`[recursiveRoutine] ${depth} --> ${packageName}@${packageVersion}`);
+  const packageDependecies = await getDependencyList(packageName, packageVersion);
+  const packageDependeciesListByName = Object.keys(packageDependecies);
+  if (packageDependeciesListByName.length === 0) return;
   
-  const data = await fetch(url)
-  const parsedData = await data.json()
-  //console.log(`[dependency-eval] Got dependencies for ${packageData.name}: ${packageData.version}`)
-  //return parsed data deps if parsedData.deps is defined, otherwise return an empty object
-  return parsedData.dependencies ? parsedData.dependencies  : {};
+  for (dependency of packageDependeciesListByName) {
+    await recursiveRoutine(dependency, packageDependecies[dependency], depth+1);
+  }
 }
 
-async function recursiveRoutine(packageData, depth){
-  console.log(`[dependency-eval] package Name:  ${depth} --> ${packageData.name}@${packageData.version}`)
-  const dependencyList = await getDependencyList(packageData)
-  //console.log(Object.keys(dependencyList))
-    if (Object.keys(dependencyList).length === 0) {
-      return
-    }
-    for (dependency of Object.keys(dependencyList)) {
-      await recursiveRoutine({'name': dependency, 'version': dependencyList[dependency]}, depth+1)
-    }
+// async function getDependenciesOfDependencies(dependencies) {
+//   depsOfDeps = {}
+//     for (const dependency in dependencies) {
+//         console.log(`[dependency-eval] grabbing dependencies for ${dependency}:${dependencies[dependency]}`)
+//         depKey = `${dependency}:${dependencies[dependency]}`
+//         const name = dependency
+//         const version = dependencies[dependency]
+//         depsOfDeps[depKey] = await getDependencyList({ name, version})
+//       }
+//   return depsOfDeps
+// }
 
-}
 
-async function getDependenciesOfDependencies(dependencies) {
-  depsOfDeps = {}
-    for (const dependency in dependencies) {
-        console.log(`[dependency-eval] grabbing dependencies for ${dependency}:${dependencies[dependency]}`)
-        depKey = `${dependency}:${dependencies[dependency]}`
-        const name = dependency
-        const version = dependencies[dependency]
-        depsOfDeps[depKey] = await getDependencyList({ name, version})
-      }
-  return depsOfDeps
-}
-
-function convertToTree() {
-  console.log('[convertToTree] lol converting to tree')
-}
-
-module.exports = { getLatestPackageVersion, fetchPackageInfo, getDependencyList, getDependenciesOfDependencies, recursiveRoutine, convertToTree}
+module.exports = { getLatestPackageVersion, fetchPackageInfo, getDependencyList, recursiveRoutine};
